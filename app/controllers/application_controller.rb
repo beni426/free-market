8fba9baf0c3b0ef297f738b21d4e47f8e8460d62
Ryu <goskn88006@yahoo.co.jp>
2022-05-05 21:58:03 +0900

    Using css to change the layout,create header and footer #7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :last_name, :first_name, :ruby_last_name, :ruby_first_name, :birthdate ])
+
+  end
 end
